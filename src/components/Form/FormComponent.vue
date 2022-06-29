1d909565d591b507b2149d3749d87290317786b9
gsprestes~ <guilherme26prestes@gmail.com>
2022-06-29 14:54:50 -0300

    FEAT: Added form component

---
+++ src/components/Form/FormComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <form id="formLogin">
+    <div class="describeComponent">
+      <TitleLogin text="Olá," type="h2" />
+      <TextComponent
+        className="regularSize subHello left"
+        text="Para continuar navegando de forma segura, efetue o login na rede."
+      />
+    </div>
+    <div class="registerComponent">
+      <TitleLogin text="Login" type="h3" />
+      <!-- <InputExemp type="email" placeholder="Usuário" />
+      <InputExemp type="password" placeholder="Senha" /> -->
+        <TextComponent className="regularSize error center" text="Ops, usuário ou senha inválidos. Tente novamente!"/>
+    </div>
+  </form>
+</template>
+
+<script>
+// import InputExemp from "../InputExemp/InputExemp.vue";
+import TitleLogin from "@/components/Title/TitleLogin.vue";
+import TextComponent from "@/components/TextComponent/TextComponent.vue";
+export default {
+  name: "FormComponent",
+  components: {
+    TextComponent,
+    TitleLogin,
+    // InputExemp,
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+@import '@/components/Form/FormComponent.scss'
+</style>
