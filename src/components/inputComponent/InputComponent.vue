cc2d040429e47078e7a2249573212d58b53a805d
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-06-29 15:39:48 -0300

    FEAT: added input component

---
+++ src/components/inputComponent/InputComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input class="input inputEmail" v-if="type === 'email'" :type="type" :placeholder="placeholder"/>
+    <input class="input inputPassword" v-else-if="type === 'password'" :type="type" :placeholder="placeholder"/>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "InputComponent",
+    props:{
+        type: {
+            type: String
+        },
+        nameClass: {
+            type: String,
+        },
+        placeholder:{
+            type: String,
+            default: 'null',
+        },
+    },
+}
+</script>
+
+<style lang="sass" scoped>
+  @import '@/components/inputComponent/InputComponent.scss'
+</style>
