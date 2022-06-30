755829f62a37fdf797903631f969544a71dc7045
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-06-30 17:07:05 -0300

    FEAT: Creating local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,40 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    
+    name: "",
+    password: "",
   },
-  getters: {
-  },
-  mutations: {
 
+  getters: {},
+  mutations: {
+    setName(state, name) {
+      state.name = name;
+      if (/^(\w)+\.(\w)+$/.test(name)) {
+        localStorage.setItem("name", name);
+      } else {
+        localStorage.setItem("name", "");
+      }
+    },
+    setPassword(state, password) {
+      state.password = password;
+      if (/^\d{6,9}$/.test(password)) {
+        localStorage.setItem("password", password);
+      } else {
+        localStorage.setItem("password", "");
+      }
+    },
   },
   actions: {
-    
-    
+    setName({ commit }, name) {
+      commit("setName", name);
+    },
+    setPassword({ commit }, password) {
+      commit("setPassword", password);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
