138dcbc0a8c2123ea1f99c15947896560c50ac88
gsprestes~ <guilherme26prestes@gmail.com>
2022-06-30 14:51:12 -0300

    FEAT/STYLE: created button component and added stylization

---
+++ src/components/Button/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :class="classButton">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: "ButtonComponent",
+    props: {
+        text: {
+            type: String,
+            default: ""
+        },
+        classButton: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+    @import '@/components/Button/ButtonComponent.scss'
+</style>
