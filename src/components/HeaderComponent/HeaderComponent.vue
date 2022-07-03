0601d7ca7bfb23ae83d29265b87b10383d40ef3f
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2022-07-02 23:25:09 -0300

    STYLE: stylizing header component

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -1,8 +1,14 @@
 <template>
-    <header>
-        <img :src="require('../../assets/logo-black.png')" alt="" class="logo-black" />
-        <ClockComponent />
-        <WeatherComponent />
+    <header class="header">
+        <div class="logo-container">
+            <img :src="require('../../assets/logo-black.png')" alt="" class="logo-black" />
+        </div>
+        <div class="clock-container">
+            <ClockComponent />
+        </div>
+        <div class="weather-container">
+            <WeatherComponent />
+        </div>
     </header>
 </template>
 
@@ -18,6 +24,6 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import './HeaderComponent.scss';
 </style>
