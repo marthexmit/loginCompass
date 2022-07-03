2e83338b796930b4bd58dba9a3d6089cc0af3828
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2022-07-02 22:36:23 -0300

    FEAT: structuring header component

---
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <img :src="require('../../assets/logo-black.png')" alt="" class="logo-black" />
+        <ClockComponent />
+        <WeatherComponent />
+    </header>
+</template>
+
+<script>
+import ClockComponent from '../ClockComponent/ClockComponent.vue';
+import WeatherComponent from '../WeatherComponent/WeatherComponent.vue';
+export default {
+    name: "HeaderComponent",
+    components: {
+        ClockComponent,
+        WeatherComponent,
+    }
+}
+</script>
+
+<style>
+
+</style>
