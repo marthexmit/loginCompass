a4b30d8078a4195d31d9becb935fd942226a10c4
gsprestes~ <guilherme26prestes@gmail.com>
2022-07-04 00:37:46 -0300

    FEAT: created HomeFooter component

---
+++ src/components/HomeFooter/HomeFooter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="separation">
+        <div class="">
+            <TextComponent text="Essa janela do navegador é usada para manter sua sessão de autenticação ativa. Deixe-a aberta em segundo plano e abra uma nova janela para continuar a navegar." />
+        </div>
+        <ButtonComponent />
+        <ButtonComponent />
+    </div>
+</template>
+
+<script>
+import TextComponent from '@/components/TextComponent/TextComponent.vue';
+import ButtonComponent from '@/components/Button/ButtonComponent.vue';
+export default {
+    name: "HomeFooter",
+    components: {
+        TextComponent,
+        ButtonComponent
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './HomeFooter.scss';
+</style>
